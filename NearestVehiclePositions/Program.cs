48016400092fb9dc401ef99c63eb43c04477df23
khayalethu <khayalethusofohlo@gmail.com>
2023-07-18 21:46:41 +0200

    Add project files.

---
+++ NearestVehiclePositions/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NearestVehiclePositions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading data and processing... Please wait.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string filePath = "VehiclePositions.dat";
+
+            // Creating the bounds for the Quadtree
+            Quadtree.Rectangle quadtreeBounds = new Quadtree.Rectangle(-90, -180, 90, 180);
+
+            // Creating the Quadtree instance with the specified bounds
+            Quadtree quadtree = new Quadtree(quadtreeBounds);
+
+            // Loading vehicle positions from the binary file and insert into the Quadtree
+            LoadAndInsertVehiclePositions(filePath, quadtree);
+
+            // The 10 coordinates to find the nearest vehicle positions
+            List<Quadtree.Point> coordinates = new List<Quadtree.Point>
+            {
+                new Quadtree.Point(34.544909f, -102.100843f),
+                new Quadtree.Point(32.345544f, -99.123124f),
+                new Quadtree.Point(33.234235f, -100.214124f),
+                new Quadtree.Point(35.195739f, -95.348899f),
+                new Quadtree.Point(31.895839f, -97.789573f),
+                new Quadtree.Point(32.895839f, -101.789573f),
+                new Quadtree.Point(34.115839f, -100.225732f),
+                new Quadtree.Point(32.335839f, -99.992232f),
+                new Quadtree.Point(33.535339f, -94.792232f),
+                new Quadtree.Point(32.234235f, -100.222222f)
+            };
+
+            // Finding the nearest vehicle positions for the given coordinates
+            foreach (var coordinate in coordinates)
+            {
+                var nearestPosition = quadtree.FindNearest(coordinate);
+
+                if (nearestPosition != null)
+                {
+                    Console.WriteLine($"Nearest vehicle position to target point ({coordinate.Latitude}, {coordinate.Longitude}):");
+                    Console.WriteLine($"Vehicle ID: {nearestPosition.VehicleId}");
+                    Console.WriteLine($"Vehicle Registration: {nearestPosition.VehicleRegistration}");
+                    Console.WriteLine($"Latitude: {nearestPosition.Position.Latitude}");
+                    Console.WriteLine($"Longitude: {nearestPosition.Position.Longitude}");
+                    Console.WriteLine();
+                }
+            }
+
+            // Await user input before closing the console
+            Console.ReadLine();
+        }
+
+        static void LoadAndInsertVehiclePositions(string filePath, Quadtree quadtree)
+        {
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[4]; // Buffer for reading 4-byte chunks (Int32)
+                byte[] floatBuffer = new byte[4]; // Buffer for reading 4-byte chunks (Float)
+                byte[] ulongBuffer = new byte[8]; // Buffer for reading 8-byte chunks (UInt64)
+
+                while (fileStream.Read(buffer, 0, 4) == 4) // Read 4 bytes for VehicleId
+                {
+                    int vehicleId = BitConverter.ToInt32(buffer, 0);
+
+                    // Read and interpret the VehicleRegistration as a null-terminated ASCII string
+                    StringBuilder registrationBuilder = new StringBuilder();
+                    byte registrationByte;
+                    while ((registrationByte = (byte)fileStream.ReadByte()) != 0)
+                    {
+                        registrationBuilder.Append((char)registrationByte);
+                    }
+                    string vehicleRegistration = registrationBuilder.ToString();
+
+                    // Read 4 bytes for Latitude (float)
+                    fileStream.Read(floatBuffer, 0, 4);
+                    float latitude = BitConverter.ToSingle(floatBuffer, 0);
+
+                    // Read 4 bytes for Longitude (float)
+                    fileStream.Read(floatBuffer, 0, 4);
+                    float longitude = BitConverter.ToSingle(floatBuffer, 0);
+
+                    // Read 8 bytes for RecordedTimeUTC (ulong)
+                    fileStream.Read(ulongBuffer, 0, 8);
+                    ulong recordedTimeUTC = BitConverter.ToUInt64(ulongBuffer, 0);
+
+                    Quadtree.Point position = new Quadtree.Point(latitude, longitude);
+
+                    Quadtree.VehiclePosition vehiclePosition = new Quadtree.VehiclePosition
+                    {
+                        VehicleId = vehicleId,
+                        VehicleRegistration = vehicleRegistration,
+                        Position = position,
+                        RecordedTimeUTC = recordedTimeUTC
+                    };
+
+                    quadtree.Insert(vehiclePosition);
+                }
+            }
+        }
+    }
+}
---
+++ NearestVehiclePositions/Quadtree.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NearestVehiclePositions
+{
+    internal class Quadtree
+    {
+        public class Rectangle
+        {
+            public float MinX { get; }
+            public float MinY { get; }
+            public float MaxX { get; }
+            public float MaxY { get; }
+
+            public Rectangle(float minX, float minY, float maxX, float maxY)
+            {
+                MinX = minX;
+                MinY = minY;
+                MaxX = maxX;
+                MaxY = maxY;
+            }
+
+            public bool Contains(Point point)
+            {
+                return point.Latitude >= MinX && point.Latitude <= MaxX &&
+                       point.Longitude >= MinY && point.Longitude <= MaxY;
+            }
+        }
+
+        public class Point
+        {
+            public float Latitude { get; }
+            public float Longitude { get; }
+
+            public Point(float latitude, float longitude)
+            {
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+        }
+
+        public class VehiclePosition
+        {
+            public int VehicleId { get; set; }
+            public string VehicleRegistration { get; set; }
+            public Point Position { get; set; }
+            public ulong RecordedTimeUTC { get; set; }
+        }
+
+        // Quadtree implementation
+        private readonly int _nodeCapacity;
+        private readonly Rectangle _bounds;
+        private readonly List<VehiclePosition> _positions;
+        private Quadtree _northWest;
+        private Quadtree _northEast;
+        private Quadtree _southWest;
+        private Quadtree _southEast;
+
+        public Quadtree(Rectangle bounds, int nodeCapacity = 4)
+        {
+            _bounds = bounds;
+            _nodeCapacity = nodeCapacity;
+            _positions = new List<VehiclePosition>();
+            _northWest = null;
+            _northEast = null;
+            _southWest = null;
+            _southEast = null;
+        }
+
+        public void Insert(VehiclePosition vehiclePosition)
+        {
+            // If the point is not within the Quadtree bounds, ignore it
+            if (!_bounds.Contains(vehiclePosition.Position))
+                return;
+
+            // If the node has capacity, add the position to the current node
+            if (_positions.Count < _nodeCapacity)
+            {
+                _positions.Add(vehiclePosition);
+            }
+            else
+            {
+                // Subdivide the current node if not already divided
+                if (_northWest == null)
+                    Subdivide();
+
+                // Insert the position into the appropriate sub-node
+                if (_northWest._bounds.Contains(vehiclePosition.Position))
+                    _northWest.Insert(vehiclePosition);
+                else if (_northEast._bounds.Contains(vehiclePosition.Position))
+                    _northEast.Insert(vehiclePosition);
+                else if (_southWest._bounds.Contains(vehiclePosition.Position))
+                    _southWest.Insert(vehiclePosition);
+                else if (_southEast._bounds.Contains(vehiclePosition.Position))
+                    _southEast.Insert(vehiclePosition);
+            }
+        }
+
+        public VehiclePosition FindNearest(Point targetPosition)
+        {
+            float nearestDistanceSquared = float.MaxValue;
+            VehiclePosition nearestVehicle = null;
+
+            // Start with the root node
+            FindNearestRecursive(this, targetPosition, ref nearestDistanceSquared, ref nearestVehicle);
+
+            return nearestVehicle;
+        }
+
+        private void FindNearestRecursive(Quadtree node, Point targetPosition, ref float nearestDistanceSquared, ref VehiclePosition nearestVehicle)
+        {
+            foreach (var vehicle in node._positions)
+            {
+                float distanceSquared = CalculateDistanceSquared(vehicle.Position, targetPosition);
+                if (distanceSquared < nearestDistanceSquared)
+                {
+                    nearestDistanceSquared = distanceSquared;
+                    nearestVehicle = vehicle;
+                }
+            }
+
+            // Calculate the distance from the target point to the bounds of each sub-node
+            var distanceToNorthWest = CalculateDistanceSquared(node._northWest?._bounds, targetPosition);
+            var distanceToNorthEast = CalculateDistanceSquared(node._northEast?._bounds, targetPosition);
+            var distanceToSouthWest = CalculateDistanceSquared(node._southWest?._bounds, targetPosition);
+            var distanceToSouthEast = CalculateDistanceSquared(node._southEast?._bounds, targetPosition);
+
+            // Recursively search the sub-nodes that are closer to the target point first
+            if (node._northWest != null && distanceToNorthWest < nearestDistanceSquared)
+                FindNearestRecursive(node._northWest, targetPosition, ref nearestDistanceSquared, ref nearestVehicle);
+
+            if (node._northEast != null && distanceToNorthEast < nearestDistanceSquared)
+                FindNearestRecursive(node._northEast, targetPosition, ref nearestDistanceSquared, ref nearestVehicle);
+
+            if (node._southWest != null && distanceToSouthWest < nearestDistanceSquared)
+                FindNearestRecursive(node._southWest, targetPosition, ref nearestDistanceSquared, ref nearestVehicle);
+
+            if (node._southEast != null && distanceToSouthEast < nearestDistanceSquared)
+                FindNearestRecursive(node._southEast, targetPosition, ref nearestDistanceSquared, ref nearestVehicle);
+        }
+
+        private void Subdivide()
+        {
+            float xMid = (_bounds.MinX + _bounds.MaxX) / 2f;
+            float yMid = (_bounds.MinY + _bounds.MaxY) / 2f;
+
+            _northWest = new Quadtree(new Rectangle(_bounds.MinX, _bounds.MinY, xMid, yMid), _nodeCapacity);
+            _northEast = new Quadtree(new Rectangle(xMid, _bounds.MinY, _bounds.MaxX, yMid), _nodeCapacity);
+            _southWest = new Quadtree(new Rectangle(_bounds.MinX, yMid, xMid, _bounds.MaxY), _nodeCapacity);
+            _southEast = new Quadtree(new Rectangle(xMid, yMid, _bounds.MaxX, _bounds.MaxY), _nodeCapacity);
+        }
+
+        private float CalculateDistanceSquared(Point p1, Point p2)
+        {
+            float latDiff = p2.Latitude - p1.Latitude;
+            float lonDiff = p2.Longitude - p1.Longitude;
+            return (latDiff * latDiff) + (lonDiff * lonDiff);
+        }
+
+        private float CalculateDistanceSquared(Rectangle bounds, Point point)
+        {
+            float dx = Math.Max(Math.Abs(point.Latitude - bounds.MinX), Math.Abs(point.Latitude - bounds.MaxX));
+            float dy = Math.Max(Math.Abs(point.Longitude - bounds.MinY), Math.Abs(point.Longitude - bounds.MaxY));
+            return (dx * dx) + (dy * dy);
+        }
+    }
+}
